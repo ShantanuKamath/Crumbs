735d0c99d7b4f19b7c8b7fb69914f1caf35f2aa8
singhpriyanshu5 <singhpriyanshu5@gmail.com>
2016-02-14 00:27:22 +0800

    added facebook login

--- app/src/main/java/com/example/priyanshu/crumbs/MainActivity.java
+++ app/src/main/java/com/example/priyanshu/crumbs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.priyanshu.crumbs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 import com.parse.ui.ParseLoginBuilder;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,11 +19,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.priyanshu.crumbs",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
 
         Parse.initialize(this);
+        ParseFacebookUtils.initialize(this);
         ParseLoginBuilder builder = new ParseLoginBuilder(this);
         startActivityForResult(builder.build(), 0);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
