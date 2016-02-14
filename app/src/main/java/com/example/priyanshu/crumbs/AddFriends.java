19676eb75068f4cbcee92797e7d919d1e9cdffbe
pratyumjagannath <pratyum96@gmail.com>
2016-02-15 02:08:19 +0800

    Friends List + Restaurant List done

--- app/src/main/java/com/example/priyanshu/crumbs/AddFriends.java
+++ app/src/main/java/com/example/priyanshu/crumbs/AddFriends.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -31,7 +33,7 @@ public class AddFriends extends AppCompatActivity {
         query.whereNotEqualTo("username", "");
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
-            public void done(List<ParseUser> objects, ParseException e) {
+            public void done(final List<ParseUser> objects, ParseException e) {
                 if (e == null) {
                     Log.d("FRIENDS", String.valueOf(objects.size()));
                     users.addAll(objects);
@@ -62,6 +64,27 @@ public class AddFriends extends AppCompatActivity {
                     actv.setAdapter(friends_list_adapter);
                     actv.setThreshold(1);
                     lv.setAdapter(friends_list_adapter);
+                    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            ParseUser user = ParseUser.getCurrentUser();
+                            ArrayList<String> friends_ar = (ArrayList<String>) user.get("friends");
+                            if (friends_ar == null){
+                                friends_ar = new ArrayList<String>();
+                            }
+                            if (friends_ar.contains(objects.get((int)id).get("name"))) {
+                                Toast toast = Toast.makeText(getApplicationContext(), "Already There!", Toast.LENGTH_SHORT);
+                                toast.show();
+                            } else {
+//                                        Log.d(LOG_TAG,friends_ar.toString());
+                                friends_ar.add((String)objects.get((int)id).get("name"));
+                                user.put("friends", friends_ar);
+                                user.saveInBackground();
+                                Toast toast = Toast.makeText(getApplicationContext(), "Friend Added!", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        }
+                    });
 
                 } else {
                     e.printStackTrace();
