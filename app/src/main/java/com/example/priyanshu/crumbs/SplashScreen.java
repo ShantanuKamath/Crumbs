78136f17d4da0477f932e91571c46a5d0ff72ea8
pratyumjagannath <pratyum96@gmail.com>
2016-02-14 18:40:33 +0800

    Splashscreen Fixed !

--- app/src/main/java/com/example/priyanshu/crumbs/SplashScreen.java
+++ app/src/main/java/com/example/priyanshu/crumbs/SplashScreen.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -16,7 +17,9 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         final ImageView iv = (ImageView) findViewById(R.id.pizza);
         final ImageView logo = (ImageView) findViewById(R.id.logo);
+        final TextView crumbs = (TextView) findViewById(R.id.crumbs_tv);
         logo.setVisibility(View.INVISIBLE);
+        crumbs.setVisibility(View.INVISIBLE);
         Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.move_up);
         final Animation fade_in = AnimationUtils.loadAnimation(getBaseContext(),R.anim.fade_in);
         final Animation fade_out = AnimationUtils.loadAnimation(getBaseContext(),R.anim.fade_out);
@@ -33,9 +36,25 @@ public class SplashScreen extends AppCompatActivity {
                 iv.startAnimation(fade_out);
                 logo.setVisibility(View.VISIBLE);
                 logo.startAnimation(fade_in);
-                finish();
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(i);
+                fade_out.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        crumbs.setVisibility(View.INVISIBLE);
+                        crumbs.startAnimation(fade_in);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        finish();
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
             }
 
             @Override
