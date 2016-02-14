539e370f68679d89e7299b26529011b18ca10597
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-02-15 00:29:26 +0800

    fix up main

--- app/src/main/java/com/example/priyanshu/crumbs/MainActivity.java
+++ app/src/main/java/com/example/priyanshu/crumbs/MainActivity.java
@@ -3,96 +3,52 @@ package com.example.priyanshu.crumbs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
-import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 
-import java.util.HashMap;
-
-public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
+public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
     private SliderLayout mDemoSlider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//                try {
-//            PackageInfo info = getPackageManager().getPackageInfo(
-//                    "com.example.priyanshu.health2u",
-//                    PackageManager.GET_SIGNATURES);
-//            for (Signature signature : info.signatures) {
-//                MessageDigest md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//            }
-//        } catch (PackageManager.NameNotFoundException e) {
-//
-//        } catch (NoSuchAlgorithmException e) {
-//
-//        }
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         Parse.initialize(this);
         ParseFacebookUtils.initialize(this);
-        if(ParseUser.getCurrentUser()==null) {
+        if (ParseUser.getCurrentUser() == null) {
             ParseLoginBuilder builder = new ParseLoginBuilder(this);
             startActivityForResult(builder.build(), 0);
         }
-
-        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
-
-        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("@Mia Pizzeria: 15% off on medium-sized pizzas",R.drawable.e);
-        file_maps.put("@Capricci: 1 for 1 weekend offer",R.drawable.c);
-        file_maps.put("@The Living Café: BLT meal for $15 only",R.drawable.a);
-        file_maps.put("@Sam's Deli: Grilled steak sandwich @$14", R.drawable.b);
-
-        for(String name : file_maps.keySet()){
-            TextSliderView textSliderView = new TextSliderView(this);
-            // initialize a SliderLayout
-            textSliderView
-                    .description(name)
-                    .image(file_maps.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit)
-                    .setOnSliderClickListener(this);
-
-            //add your extra information
-            textSliderView.bundle(new Bundle());
-            textSliderView.getBundle()
-                    .putString("extra",name);
-
-            mDemoSlider.addSlider(textSliderView);
+        if(ParseUser.getCurrentUser()!=null) {
+            Intent intent = new Intent(this, TabBarActivity.class);
+            startActivity(intent);
         }
-        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Stack);
-        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
-        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
-        mDemoSlider.setDuration(4000);
-        mDemoSlider.addOnPageChangeListener(this);
     }
 
-    @Override
-    protected void onStop() {
-        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
-        mDemoSlider.stopAutoCycle();
-        super.onStop();
-    }
+//    @Override
+//    protected void onStop() {
+//        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+//        mDemoSlider.stopAutoCycle();
+//        super.onStop();
+//    }
 
     @Override
     public void onSliderClick(BaseSliderView slider) {
 //        Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
     }
+
     @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
 
     @Override
     public void onPageSelected(int position) {
@@ -100,13 +56,16 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
     }
 
     @Override
-    public void onPageScrollStateChanged(int state) {}
+    public void onPageScrollStateChanged(int state) {
+    }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+        Intent intent = new Intent(this, TabBarActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -126,8 +85,7 @@ public class MainActivity extends AppCompatActivity implements BaseSliderView.On
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        else if (id == R.id.action_log_out) {
+        } else if (id == R.id.action_log_out) {
             ParseUser.logOut();
             ParseLoginBuilder builder = new ParseLoginBuilder(this);
             startActivityForResult(builder.build(), 0);
